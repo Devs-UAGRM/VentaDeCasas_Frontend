0ac2fcde7b8025c67b8a2d61180e11309f7eb4d4
runinbk <runinbk@gmail.com>
2023-07-18 14:43:13 -0400

    Rutas hijas - authModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    DashboardPageComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  // error 404
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+
+  // default page
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  },
+
+  // otherwise redirect to auth page
+  {
+    path: '**',
+    redirectTo: '404'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
