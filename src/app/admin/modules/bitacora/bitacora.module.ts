b4113c791fe9786311b5079c3f05f0317959be42
runinbk <runinbk@gmail.com>
2023-07-18 14:15:54 -0400

    Creacion de modulos hijos del modulo padre auth

---
+++ src/app/admin/modules/bitacora/bitacora.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BitacoraRoutingModule } from './bitacora-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BitacoraRoutingModule
+  ]
+})
+export class BitacoraModule { }
