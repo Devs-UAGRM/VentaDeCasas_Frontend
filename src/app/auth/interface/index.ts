f84e9fe3777d69e383b3a6c50c8c85c58037385a
runinbk <runinbk@gmail.com>
2023-07-19 08:20:29 -0400

    interfaces types

---
+++ src/app/auth/interface/index.ts
@@ -0,0 +1,4 @@
+
+
+export * from './login-response.interface';
+export * from './auth-status.enum';
---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+
+export interface User {
+    id: number;
+    nombre: string;
+    correo: string;
+    password: string;
+    img: null;
+    celular: number;
+    google: boolean;
+    estado: boolean;
+    id_rol: number;
+    createdAt: string;
+    updatedAt: string;
+}
