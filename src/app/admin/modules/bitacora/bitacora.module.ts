133b520e49b01135d77aff8413319f48d801d086
runinbk <runinbk@gmail.com>
2023-07-19 23:56:55 -0400

    Error sidenav

--- src/app/admin/modules/bitacora/bitacora.module.ts
+++ src/app/admin/modules/bitacora/bitacora.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BitacoraRoutingModule } from './bitacora-routing.module';
 import { BitacoraLayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
+import { BitacoraListPageComponent } from './pages/list-page/list-page.component';
 import { SidenavComponent } from '../../shared/sidenav-component/sidenav-component.component';
 
 
 @NgModule({
   declarations: [
     BitacoraLayoutPageComponent,
-    ListPageComponent
+    BitacoraListPageComponent
   ],
   imports: [
     CommonModule,
